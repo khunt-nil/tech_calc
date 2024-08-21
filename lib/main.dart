44c9cdaa1d119278dd6b39e42d8b381c95a75536
Nil Khunt <132322506+khunt-nil@users.noreply.github.com>
2024-08-21 05:36:31 +0530

    modify the view of the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
-import 'dart:math';
 
 void main() {
   runApp(TechCalcApp());
 }
 
 class TechCalcApp extends StatelessWidget {
+  const TechCalcApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,6 +21,8 @@ class TechCalcApp extends StatelessWidget {
 }
 
 class CalculatorHome extends StatefulWidget {
+  const CalculatorHome({super.key});
+
   @override
   _CalculatorHomeState createState() => _CalculatorHomeState();
 }
@@ -32,7 +33,7 @@ class _CalculatorHomeState extends State<CalculatorHome> {
 
   void _onButtonPressed(String value) {
     setState(() {
-      if (value == 'C') {
+      if (value == 'AC') {
         expression = '';
         result = '';
       } else if (value == '=') {
@@ -41,36 +42,17 @@ class _CalculatorHomeState extends State<CalculatorHome> {
           Expression exp = parser.parse(expression);
           ContextModel cm = ContextModel();
           result = '${exp.evaluate(EvaluationType.REAL, cm)}';
-        } catch (error) {
+        } catch (e) {
           result = 'Error';
         }
-      } else if (value == '!') {
-        try {
-          int number = int.parse(expression);
-          result = '${_Factorial(number)}';
-        } catch (error) {
-          result = 'Error';
+      } else if (value == '⌫') {
+        if (expression.isNotEmpty) {
+          expression = expression.substring(0, expression.length - 1);
         }
-      } else if (value == 'log') {
-        try {
-          double number = double.parse(expression);
-          result = '${log(number)}';
-        } catch (error) {
-          result = 'Error';
-        }
-      } else if (value == 'e') {
-        expression +=
-            '2.718281828459045'; // Approximate value of Euler's number
       } else {
         expression += value;
       }
     });
-  }
-
-  // ignore: non_constant_identifier_names
-  int _Factorial(int n) {
-    if (n <= 1) return 1;
-    return n * _Factorial(n - 1);
   }
 
   @override
@@ -87,8 +69,7 @@ class _CalculatorHomeState extends State<CalculatorHome> {
               alignment: Alignment.bottomRight,
               child: Text(
                 expression,
-                style:
-                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
           ),
@@ -98,8 +79,7 @@ class _CalculatorHomeState extends State<CalculatorHome> {
               alignment: Alignment.bottomRight,
               child: Text(
                 result,
-                style:
-                    const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
               ),
             ),
           ),
@@ -113,12 +93,11 @@ class _CalculatorHomeState extends State<CalculatorHome> {
   Widget _buildButtons() {
     return Column(
       children: [
-        _buildButtonRow(['7', '8', '9', '/']),
-        _buildButtonRow(['4', '5', '6', '*']),
-        _buildButtonRow(['1', '2', '3', '-']),
-        _buildButtonRow(['0', '.', '=', '+']),
-        _buildButtonRow(['C', '(', ')', '^']),
-        _buildButtonRow(['%', '!', 'log', 'e']), // Added log and e buttons
+        _buildButtonRow(['AC', '⌫', '%', '/']),
+        _buildButtonRow(['7', '8', '9', '*']),
+        _buildButtonRow(['4', '5', '6', '-']),
+        _buildButtonRow(['1', '2', '3', '+']),
+        _buildButtonRow(['00', '0', '.', '=']),
       ],
     );
   }
@@ -135,18 +114,18 @@ class _CalculatorHomeState extends State<CalculatorHome> {
   Widget _buildButton(String buttonText) {
     return Expanded(
       child: Container(
-        margin: EdgeInsets.all(8),
+        margin: const EdgeInsets.all(8),
         child: ElevatedButton(
           onPressed: () => _onButtonPressed(buttonText),
           style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
           ),
           child: Text(
             buttonText,
-            style: TextStyle(fontSize: 24),
+            style: const TextStyle(fontSize: 24,color: Colors.black),
           ),
         ),
       ),
